b00fd8b66aa9616cd9b666f7a665e6b68443124a
lyalka69 <l.y.a.l.k.a7.12.7@gmail.com>
2023-12-23 19:39:16 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dop2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //даны 4 целых числа. Определить сколько из них чётные.
+
+            //Console.Write("введите первое число:");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("введите второе число:");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.Write("введите третье число:");
+            //int c = int.Parse(Console.ReadLine());
+            //Console.Write("введите четвёртое число:");
+            //int d = int.Parse(Console.ReadLine());
+            //int e = 0;
+            //if (a % 2 == 0) 
+            //{
+            //    e++;
+            //}
+            //if (b % 2 == 0)
+            //{
+            //    e++;
+            //}
+            //if (c % 2 == 0)
+            //{
+            //    e++;
+            //}
+            //if (d % 2 == 0)
+            //{
+            //    e++;
+            //}
+            //Console.Write($"количество чётных чисел: {e}");
+            //Console.ReadKey();
+
+            //даны 4 целых числа. Определить сколько из них нечётные.
+
+            //Console.Write("введите первое число:");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("введите второе число:");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.Write("введите третье число:");
+            //int c = int.Parse(Console.ReadLine());
+            //Console.Write("введите четвёртое число:");
+            //int d = int.Parse(Console.ReadLine());
+            //int e = 0;
+            //if (a % 2 != 0)
+            //{
+            //    e++;
+            //}
+            //if (b % 2 != 0)
+            //{
+            //    e++;
+            //}
+            //if (c % 2 != 0)
+            //{
+            //    e++;
+            //}
+            //if (d % 2 != 0)
+            //{
+            //    e++;
+            //}
+
+            //Console.Write($"количество нечётных чисел: {e}");
+            //Console.ReadKey();
+
+
+
+
+
+
+
+        }
+    }
+}
